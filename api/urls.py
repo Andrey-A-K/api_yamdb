80d612781f6e551fef6106b7580ecf6c2db54745
Vitalii Tsyoma <vitaliytsyoma@icloud.com>
2021-06-16 21:56:55 +0300

    test3

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-<<<<<<< HEAD
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import ReviewsViewSet, CommentViewSet
+from .views import ReviewsViewSet, CommentViewSet, TitlesViewSet, CategoriesViewSet, GenresViewSet
 
 
 router_v1 = DefaultRouter()
@@ -16,33 +15,9 @@ router_v1.register(
     CommentViewSet,
     basename='api_comments'
 )
-
+router_v1.register('titles', TitlesViewSet, basename='titles')
+router_v1.register('categories', CategoriesViewSet, basename='categories')
+router_v1.register('genres', GenresViewSet, basename='genres')
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
-=======
-from django.urls import include
-from rest_framework.authtoken import views
-from rest_framework.routers import DefaultRouter
-from .views import TitlesViewSet
-from .views import CategoriesViewSet
-from .views import GenresViewSet
-
-# Создаётся роутер
-router = DefaultRouter()
-# Связываем URL с viewset, аналогично обычному path()
-# В роутере можно зарегистрировать любое количество пар "URL, viewset":
-# router.register('categories/<slug:slug>/',
-#                 CategoriesViewSet,
-#                 basename='category')
-router.register('categories', CategoriesViewSet, basename='categories')
-router.register('genres', GenresViewSet, basename='genres')
-router.register('titles', TitlesViewSet, basename='titles')
-
-
-urlpatterns = [
-    # В список добавляем новый path() с роутером.
-    # Все зарегистрированные в router пути доступны в router.urls
-    path('v1/', include(router.urls)),
-    path('v1/api-token-auth/', views.obtain_auth_token),
->>>>>>> 68454656fb98752b7f6d21a97bdbad61ec58e500
 ]
