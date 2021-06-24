95ca097d628fd8603aa356014c0410aa7d69de58
Andrey-A-K <andrey-promalp@yandex.ru>
2021-06-24 18:23:35 +0300

    перенёс валидатор в отдельный файл

--- api/models.py
+++ api/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-import datetime
-from django.core.exceptions import ValidationError
-
-
-def validate_year(value):
-    year_date = datetime.date.today().year
-    if not (0 < value <= year_date):
-        raise ValidationError('Введите корректный год')
-    return value
+from .validators import validate_year
 
 
 class User(AbstractUser):
